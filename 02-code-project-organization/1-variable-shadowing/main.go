a27532032e7583d78ad45b0aa63749a9163b6f0e
文徐 <wangwenxue.wwx@alibaba-inc.com>
2022-11-21 11:22:27 +0800

    update 1 chapters

--- 02-code-project-organization/1-variable-shadowing/main.go
+++ 02-code-project-organization/1-variable-shadowing/main.go
@@ -5,6 +5,12 @@ import (
 	"net/http"
 )
 
+/*
+问题描述：
+	if中的代码块重新定义了名为client的变量，即最外层的client和if里面的client是不同的两个变量，当你
+	在后面使用client的时候，最外层的client仍为nil
+*/
+
 func listing1() error {
 	var client *http.Client
 	if tracing {
@@ -24,6 +30,11 @@ func listing1() error {
 	_ = client
 	return nil
 }
+
+/*
+解决方案1：
+	可以通过将代码块中的变量重新命名为和外层的client不同名避免上述nil的问题
+*/
 
 func listing2() error {
 	var client *http.Client
@@ -45,6 +56,11 @@ func listing2() error {
 	return nil
 }
 
+/*
+解决方案2：
+	提前将变量都声明好，缺点就是需要提前声明
+*/
+
 func listing3() error {
 	var client *http.Client
 	var err error
@@ -64,6 +80,10 @@ func listing3() error {
 	return nil
 }
 
+/*
+解决方案3：
+	将err的处理从嵌套语句中提取出来，减少代码的嵌入，可以让代码的阅读更简单
+*/
 func listing4() error {
 	var client *http.Client
 	var err error
